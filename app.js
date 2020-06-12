6d075810af38db03c99642559d834df2398168ed
丁玉娇 <dingyujiao@syhealth.com>
2020-06-12 16:48:09 +0800

    添加压缩等

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
-import './test.txt';
-import './less/myLess.css';
-module.exports = function(){
-	console.log('hhh222')
+import './assets/test.txt';
+import './less/myLess.less';
+const json = require('./assets/text.md');
+
+
+function test(){
+	// 哈哈哈
+	console.log('hhh222',json);
 }
+
+export default test()
--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // html模板
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // css 分离
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // css压缩
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 打包前清除dist
+const TerserPlugin = require('terser-webpack-plugin'); // 压缩js插件
+
+
 module.exports = {
 	mode: 'none',
 	context: __dirname,
 	entry: path.resolve(__dirname , 'app.js'),
 	output: {
 		path: path.resolve(__dirname , './dist'),
-		filename: 'output.js'
+		filename: '[name].[hash].js',
+		publicPath: './'
 	},
+	
 	module:{
 		rules: [
 			{ test: /\.txt$/, use: 'raw-loader' },
-			{ test: /\.css$/, use: [
+			{ test: /\.(less|css)$/, use: [
 				MiniCssExtractPlugin.loader, // 因为分离了css，所以需要添加这个
 				{
 					loader: 'css-loader'
+				},{
+					loader: 'less-loader'
+				},{
+					loader:'postcss-loader',
+					options: {
+						env: 'development',
+						config: {
+						  ctx: {
+							// cssnext: {...options},
+							// cssnano: {...options},
+							autoprefixer: {flexbox:true}
+						  }
+						}
+					  }
 				}
-			] }
+			] },
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+				  loader: 'babel-loader',
+				  options: {
+					presets: ['@babel/preset-env']
+				  }
+				}
+			},
+			{
+				test: /\.md$/,
+				use: [ 'json-loader', 'yaml-frontmatter-loader' ]
+			 }
 		]
 	},
 	plugins: [
-			new HtmlWebpackPlugin({
-				template: './index.html',
-				title: 'hhhhh'
-			}),
-			new OptimizeCSSAssetsPlugin(),
-			new MiniCssExtractPlugin({
-				filename: 'css/[name].[hash].css',
-				chunkFilename: 'css/[name].[hash].css',
-			})
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+		new OptimizeCSSAssetsPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[hash].css',
+			chunkFilename: 'css/[name].[hash].css',
+		}),
+			
 	],
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserPlugin()],
+	},
 	devServer: {
 		contentBase: path.join(__dirname, "dist"),
-		compress: true,
 		port: 9000
 	}
 }
